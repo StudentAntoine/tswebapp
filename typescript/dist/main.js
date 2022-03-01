4fd11d04059d56de0fdfc882187b74e8b336fc56
StudentAntoine <louis.antoine231@gmail.com>
2022-03-01 14:30:41 +0100

    Setup + First Main

---
+++ typescript/dist/main.js
@@ -0,0 +1,5 @@
+"use strict";
+function hello(compiler) {
+    return "Hello from ".concat(compiler);
+}
+console.log(hello("TypeScript"));
---
+++ typescript/src/main.ts
@@ -0,0 +1,4 @@
+function hello(compiler: string) {
+    return `Hello from ${compiler}`
+}
+console.log(hello("TypeScript"))
