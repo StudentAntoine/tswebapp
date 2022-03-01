530af9969149bc3330360b870a67088b751cbfda
StudentAntoine <louis.antoine231@gmail.com>
2022-03-01 15:04:42 +0100

    Main update just 2.8

--- typescript/dist/main.js
+++ typescript/dist/main.js
@@ -1,5 +1,30 @@
 "use strict";
-function hello(compiler) {
-    return "Hello from ".concat(compiler);
-}
-console.log(hello("TypeScript"));
+var age1;
+var name1 = null;
+var Color;
+(function (Color) {
+    Color[Color["White"] = 0] = "White";
+    Color[Color["Black"] = 1] = "Black";
+    Color[Color["Brown"] = 2] = "Brown";
+    Color[Color["Red"] = 3] = "Red";
+    Color[Color["Orange"] = 4] = "Orange";
+    Color[Color["Yellow"] = 5] = "Yellow";
+    Color[Color["Green"] = 6] = "Green";
+    Color[Color["Blue"] = 7] = "Blue";
+    Color[Color["Purple"] = 8] = "Purple";
+    Color[Color["Grey"] = 9] = "Grey";
+})(Color || (Color = {}));
+var colBackground = Color.Blue;
+var PropKind;
+(function (PropKind) {
+    PropKind[PropKind["Read"] = 1] = "Read";
+    PropKind[PropKind["Write"] = 4] = "Write";
+    PropKind["Indexed"] = "indexed";
+})(PropKind || (PropKind = {}));
+var kind = PropKind.Read;
+console.log(kind, PropKind[PropKind.Read]); // 1 Read
+console.log(PropKind.Write, PropKind[PropKind.Write]); // 4 Write
+console.log(PropKind.Indexed); // indexed
+var key = Symbol();
+var key2 = Symbol();
+// console.log(key == key2) // erreur
--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -1,4 +1,26 @@
-function hello(compiler: string) {
-    return `Hello from ${compiler}`
+let age1: number | undefined
+let name1: string | null = null
+
+enum Color {
+	White, Black, Brown, Red, Orange, Yellow, Green, Blue, Purple, Grey
 }
-console.log(hello("TypeScript"))
+let colBackground: Color = Color.Blue
+
+enum PropKind {
+	Read = 1,
+	Write = 4,
+	Indexed = "indexed",
+}
+let kind: PropKind = PropKind.Read
+
+console.log(kind, PropKind[PropKind.Read]) // 1 Read
+console.log(PropKind.Write, PropKind[PropKind.Write]) // 4 Write
+console.log(PropKind.Indexed) // indexed
+
+const key = Symbol()
+const key2 = Symbol()
+// console.log(key == key2) // erreur
+
+const KEY = "key"
+type key_t = "key"
+let e1: key_t = KEY
